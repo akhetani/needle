53f9c6afbfc9dccc21ce129f639901025df6741f
xsallowed <soomrotahir@yahoo.com>
2021-02-21 10:25:52 +0000

    Update parsedatafromaws.py

--- parsedatafromaws.py
+++ parsedatafromaws.py
@@ -61,11 +61,11 @@ def aws_extractevidencefiles(accesskey, secret, bucketname, evidencefolder, case
             print(file)
             if file.endswith(('.vmdk','.E01')):
                 temp_output_directory = output_directory + "/" + (file.split('.')[0])
-                os.system("mkdir " + temp_output_directory)
-                print("Extracting files from " + file)
+                os.system("mkdir " + "'" + temp_output_directory + "'")
+                print("Extracting files from :" + file)
                 print("temp output dircetory :" + temp_output_directory)
                 os.system("cd "+ dirpath)
-                os.system("7z " + "l " + "\"" + file + "\"")
+                print("7z " + "l " + "\"" + file + "\"")
                 #os.system("7z " + "x " + "'" + file + "'" + " -o/" + "'" + temp_output_directory + "' " + "[SYSTEM]/*" )
 if __name__ == "__main__":
     main(sys.argv[1:])
